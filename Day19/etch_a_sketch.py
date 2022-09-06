efa880739d6c836c22e9a4be182b8ed4a637f623
Fran Perez <franperezfolgado@outlook.com>
2022-09-06 17:09:11 +0200

    Day 29

--- Day19/etch_a_sketch.py
+++ Day19/etch_a_sketch.py
@@ -22,7 +22,7 @@ screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
 screen.onkey(key="a", fun=turn_left)
 screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="c", fun=screen.resetscreenw)
+screen.onkey(key="c", fun=screen.resetscreena)
 
 
 
